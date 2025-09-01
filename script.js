5ca3086d4e4d629d94e25d59071f837876d21689
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-09-01 12:15:20 +0530

    Updated the functionality of the rock-paper-scissors
    Created seperate functions and added event listeners to make the code better overall

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+var rock = document.createElement('button')
+rock.classList.add('rock');
+
+var rockText = document.createTextNode('Rock');
+rock.appendChild(rockText);
+
+var paper = document.createElement('button');
+paper.classList.add('paper');
+
+var paperText = document.createTextNode('Paper');
+paper.appendChild(paperText);
+
+var scissors = document.createElement('button');
+paper.classList.add('scissors');
+
+var scissorsText = document.createTextNode('Scissors');
+scissors.appendChild(scissorsText);
+
+var container = document.querySelector('.container');
+container.style.display = 'flex';
+container.style.flexDirection = 'row';
+container.style.gap = '15px';
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
+
+rock.addEventListener('click',playRound);
+paper.addEventListener('click',playRound);
+scissors.addEventListener('click',playRound);
+
 let getComputerChoice = () =>{
     let choice = Math.random();
     if(choice <= 0.3){
@@ -9,17 +39,18 @@ let getComputerChoice = () =>{
     }
 }
 
-let getHumanChoice = function(){
-    let choice = String(prompt("Enter your choice: "));
-    return choice;
+function getHumanChoice(e){
+    console.log(e.target.value);
+    return e.target.value;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(){
+function playRound(e){
     let computer = getComputerChoice();
-    let human = getHumanChoice();
+    let human = e.target.textContent;
+    console.log(human);
 
     computer = computer.toLowerCase();
     human = human.toLowerCase();
@@ -27,31 +58,49 @@ function playRound(){
     if(computer == "rock" && human == "paper"){
         console.log("Computer Wins");
         computerScore++;
+        computerScoreText.textContent = 'Computer Score: ' + computerScore +'\n';
     } else if(computer == "paper" && human == "rock"){
         console.log("Computer Wins");
         computerScore++;
+        computerScoreText.textContent = 'Computer Score: ' + computerScore +'\n';
     } else if(computer == "scissors" && human == "paper"){
         console.log("Computer wins");
         computerScore++;
+        computerScoreText.textContent = 'Computer Score: ' + computerScore +'\n';
     } else if(human == "rock" && computer == "paper"){
         console.log("You win");
         humanScore++;
+        humanScoreText.textContent = 'Human Score: ' + humanScore + '\n';
     } else if(human == "paper" && computer == "rock"){
         console.log("You win");
         humanScore++;
+        humanScoreText.textContent = 'Human Score: ' + humanScore + '\n';
     } else if(human == "scissors" && computer == "paper"){
         console.log("You win");
         humanScore++;
+        humanScoreText.textContent = 'Human Score: ' + humanScore + '\n';
     } else{
         console.log("Its a tie");
+    }
+
+    checkWinner();
+}
+
+function checkWinner(){
+    if(humanScore == 5){
+        var text = document.createTextNode('You won');
+        container.appendChild(text);
+        return;
+    } else if(computerScore == 5){
+        var text = document.createTextNode('Computer Won');
+        container.appendChild(text);
+        return;
     }
 }
 
 function playGame(){
 
-    for(let i = 0;i<5;i++){
-        playRound();
-    }
+    
 
     if(humanScore>computerScore){
         console.log("You win the game");
@@ -61,5 +110,18 @@ function playGame(){
         console.log("Tied game");
     }
 }
+
+var scoreDiv = document.createElement('div');
+scoreDiv.classList.add('scores');
+
+var humanScoreText = document.createTextNode('Your score: ' + humanScore +'\n');
+var computerScoreText = document.createTextNode('Computer score: ' + computerScore + '\n');
+
+scoreDiv.appendChild(humanScoreText);
+scoreDiv.appendChild(computerScoreText);
+
+container.appendChild(scoreDiv);
+
+
 
 playGame();
